594211aecdac7079ba379114faf23ac26bca74d6
Daudi Mwanzia <daudimwanzia@gmail.com>
2023-09-29 00:04:56 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+const findMatching = (drivers, name) => {
+    const result = drivers.filter(
+      (driver) => driver === name || driver === name.toLowerCase()
+    );
+    return result;
+};
+const fuzzyMatch = (drivers, letter) => {
+    const result = drivers.filter((driver) => {
+      const lowerCaseDriver = driver.toLowerCase();
+      const lowerCaseLetter = letter.toLowerCase(); 
+      return lowerCaseDriver.startsWith(lowerCaseLetter);
+    });
+    return result;
+};
+const matchName = (drivers, driverName) => {
+  const result = drivers.filter((driver) => driver.name === driverName);
+  return result;
+}
